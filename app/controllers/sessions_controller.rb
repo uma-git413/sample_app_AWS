fe85fe709d8415fa32f3c9f7cd7e616f9045deec
samurai <uman180920@gmail.com>
2020-01-27 20:03:04 +0000

    20/01/28_Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      #remember user
+
       redirect_to user
          
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
@@ -18,7 +22,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
